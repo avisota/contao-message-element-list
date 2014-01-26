ea42b7682b28ec7e3307b315543abd3946c265b3
Tristan Lins <tristan.lins@bit3.de>
2014-01-26 18:24:21 +0100

    Extract the list element into own project.

---
+++ module/dca/orm_avisota_message_content.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message-element-list
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Table orm_avisota_message_content
+ * Entity Avisota\Contao:MessageContent
+ */
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['metapalettes']['list'] = array
+(
+	'type'   => array('type', 'cell', 'headline'),
+	'list'   => array('listType', 'listItems'),
+	'expert' => array(':hide', 'cssID', 'space')
+);
+
+
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['listType']  = array
+(
+	'label'     => &$GLOBALS['TL_LANG']['orm_avisota_message_content']['listType'],
+	'exclude'   => true,
+	'inputType' => 'select',
+	'options'   => array('ordered', 'unordered'),
+	'reference' => &$GLOBALS['TL_LANG']['orm_avisota_message_content']
+);
+$GLOBALS['TL_DCA']['orm_avisota_message_content']['fields']['listItems'] = array
+(
+	'label'     => &$GLOBALS['TL_LANG']['orm_avisota_message_content']['listItems'],
+	'exclude'   => true,
+	'inputType' => 'listWizard',
+	'eval'      => array('allowHtml' => true)
+);
---
+++ module/languages/en/orm_avisota_message_content.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message-element-list
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['listType']     = array(
+	'List type',
+	'Please choose the type of list.'
+);
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['listItems']    = array(
+	'List items',
+	'If JavaScript is disabled, make sure to save your changes before modifying the order.'
+);
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['orm_avisota_message_content']['list_legend']      = 'List items';
